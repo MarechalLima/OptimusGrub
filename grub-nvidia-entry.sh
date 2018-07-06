fe981074c5b18c71763c3e1c09d184df6928427a
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2018-07-06 12:57:55 -0300

    personal use

--- grub-nvidia-entry.sh
+++ grub-nvidia-entry.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# https://github.com/Superdanby/Grub-Nvidia-Entry
 
 Curnel=`uname -r`
 if [[ $1 != '-f' && $1 != '--force' ]];then
@@ -52,7 +51,6 @@ exec tail -n +3 \$0
 # menu entries you want to add after this comment.  Be careful not to change
 # the 'exec tail' line above.
 `sudo sed -n '/^menuentry/,/}/p;' /boot/efi/EFI/fedora/grub.cfg | sed '/}/q' | sed 's/modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm//'`" | sudo tee /etc/grub.d/40_custom
-echo '# https://github.com/Superdanby/Grub-Nvidia-Entry' | sudo tee --append /etc/grub.d/40_custom
 
 if [[ `sudo cat /etc/grub.d/40_custom | grep rd.driver.blacklist=nouveau` == '' ]]; then
     sudo sed -i '/vmlinuz/s/$/ rd.driver.blacklist=nouveau/' /etc/grub.d/40_custom
@@ -72,19 +70,5 @@ fi
 
 sudo chmod 744 /etc/grub.d/40_custom
 sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
-
-# Runs only if the modules are unavailable.
-if [[ `sudo find /lib/ -name nvidia.ko | grep $Curnel` == '' ]]; then
-    Nvpath="/usr/src/`ls -r /usr/src/ | grep nvidia | sed -n '1p'`"
-
-    printf "\nMaking Nvidia modules...\n"
-    sudo make -C $Nvpath
-
-    printf "\nInstalling Nvidia modules...\n"
-    sudo make -C $Nvpath modules_install
-
-    printf "\nCleaning up...\n"
-    sudo make -C $Nvpath clean
-fi
 
 printf "\nSuccess! Changes will take effect on next boot."
